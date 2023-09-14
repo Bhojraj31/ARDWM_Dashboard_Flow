5e6b4b090b5b6b2bdf9f58771384e5606635e9b6
Bhojraj singh shekhawat <98007948+Bhojraj31@users.noreply.github.com>
2023-09-14 11:28:20 +0530

    Start DWM

---
+++ DWM/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text style={{fontSize:30, fontWeight:'bold'}}>
+        7773
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
